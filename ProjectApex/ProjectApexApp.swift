10ecfc7d556b4f18b07b7d4ce08dbda15315f6b9
sedwares <sahinsedar@gmail.com>
2026-07-09 18:23:01 -0400

    Initial Commit

---
+++ ProjectApex/ProjectApexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApexApp.swift
+//  ProjectApex
+//
+//  Created by sedar on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
